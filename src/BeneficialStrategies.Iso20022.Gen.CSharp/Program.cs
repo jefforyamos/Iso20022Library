6aa597660ed4ed07502f61712ffc9423ccfd897d
Jeff Ward <jeff@jeffs-mbp.attlocal.net>
2023-12-24 17:00:15 -0600

    Had to load file from external location since Github wouldn't allow size

--- src/BeneficialStrategies.Iso20022.Gen.CSharp/Program.cs
+++ src/BeneficialStrategies.Iso20022.Gen.CSharp/Program.cs
@@ -7,8 +7,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        if (args.Length < 1) throw new ArgumentException($"You must pass the folder which contains {IsoRepository.CurrentlySupportedRepoFile}");
+        var directoryInfo = new DirectoryInfo(args[0]);
+        if (!directoryInfo.Exists) throw new DirectoryNotFoundException($"Folder matching {args[0]} could not be found");
+        var file = new FileInfo(Path.Combine(directoryInfo.FullName, IsoRepository.CurrentlySupportedRepoFile));
+        if (!file.Exists) throw new FileNotFoundException($"Repo file {file.Name} not found in {directoryInfo.FullName}");
+        
+
         Console.WriteLine("Initializing repository...");
-        var repo = IsoRepository.Load();
+        var repo = IsoRepository.Load(file.FullName);
         foreach (var codeSet in repo.DataDictionary.CodeSets)
         {
             Console.WriteLine($"{codeSet.Name}");
@@ -23,7 +30,6 @@ class Program
 
         var generatorList = new List<IGenerate>
         {
-            // new ExternalCodesetGenerator(),
             new DropdownRowInterfaceGenerator(),
             new DropdownSourceInterfaceGenerator(),
             new CodesetEnumGenerator(),
