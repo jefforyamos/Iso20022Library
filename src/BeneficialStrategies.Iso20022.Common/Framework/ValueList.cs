f9845d1359bf4f3f658ce99e9c9a10be24a8f7b0
Jeff Ward <jeff@jeffs-mdp.attlocal.net>
2024-02-07 11:38:34 -0600

    Added serialization via standard interface to ValueList.

--- src/BeneficialStrategies.Iso20022.Common/Framework/ValueList.cs
+++ src/BeneficialStrategies.Iso20022.Common/Framework/ValueList.cs
@@ -1,12 +1,33 @@
-﻿namespace BeneficialStrategies.Iso20022;
+﻿using System.Xml;
+using System.Xml.Linq;
 
-  public class ValueList<T>:List<T>
+namespace BeneficialStrategies.Iso20022;
+
+/// <summary>
+/// Extends the normal list by using value-based equality.  Also uses Iso20022 serialization.
+/// </summary>
+/// <typeparam name="T">The data type of items in the collection.</typeparam>
+public class ValueList<T> : List<T>, IIsoXmlSerilizable<ValueList<T>>
+    where T: IIsoXmlSerilizable<T>
 {
     //private readonly bool _requireMathcingOrder;
     public ValueList()
     {
         
     }
+    public ValueList(IEnumerable<T> collection) : base(collection)
+    {
+    }
+
+    public ValueList(int capacity) : base(capacity)
+    {
+    }
+
+    /// <summary>
+    /// Overridden to do value-based equality.
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
     public override bool Equals(object? other)
     {
         if (other is null) return false;
@@ -24,5 +45,35 @@
         }
 
         return hashCode;
+    }
+
+    /// <summary>
+    /// Serializes the collection one item at a time based using the Iso20022 standard serialization.
+    /// </summary>
+    /// <param name="writer">The xml writer to write to.</param>
+    /// <param name="xmlNamespace">The XML namespace for the message we are currently persisting.</param>
+    public void Serialize(XmlWriter writer, string xmlNamespace)
+    {
+        foreach( var item in this)
+        {
+            item.Serialize(writer, xmlNamespace);
+        }
+    }
+
+    /// <summary>
+    /// Deserializes the collection one item at a time.
+    /// </summary>
+    /// <param name="element">Element that has the container of child elements.</param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public static ValueList<T> Deserialize(XElement element)
+    {
+        var list = new ValueList<T>();
+        foreach( var child in element.Elements())
+        {
+            var newItem = T.Deserialize(child);
+            list.Add(newItem);
+        }
+        return list;
     }
 }
