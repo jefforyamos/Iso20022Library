b45fd337171175624ee4a2e16fbe09580ac5cea3
Jeff Ward <jeff@jeffs-mdp.attlocal.net>
2024-02-12 03:59:33 -0600

    Saving Serialization POC so it doesn't get deleted.

--- src/BeneficialStrategies.Iso20022.Common.UnitTest/Framework/TestSerialization.cs
+++ src/BeneficialStrategies.Iso20022.Common.UnitTest/Framework/TestSerialization.cs
@@ -1,4 +1,8 @@
-﻿using Xunit.Abstractions;
+﻿using System.Xml;
+using System.Xml.Linq;
+using BeneficialStrategies.Iso20022.Components;
+using BeneficialStrategies.Iso20022.pain;
+using Xunit.Abstractions;
 
 namespace BeneficialStrategies.Iso20022.Framework;
 
@@ -82,4 +86,107 @@ public abstract class TestSerialization<T>
         Sample.AssertXmlSerializerDeserializesValidIso20022(ExpectedSampleXml, Output);
     }
 }
+
+public interface IBeneficialXmlWriter<TValue>
+{
+    Task WriteAsync(XmlWriter writer, TValue value, string isoNamespace);
+}
+
+// public interface IMemberDataStateXmlWriter<TMember> 
+// {
+//     Task WriteAsync(XmlWriter writer, TMember member);
+// }
+
+public class MemberWriter<TParent, TMember> : IBeneficialXmlWriter<TParent>
+{
+    private readonly string elementName;
+    private readonly IBeneficialXmlWriter<TMember> dataStateWriter;
+    private readonly Func<TParent, TMember?> propertyGetter;
+
+    public MemberWriter(string elementName, IBeneficialXmlWriter<TMember> dataStateWriter, Func<TParent,TMember?> propertyGetter)
+    {
+        this.elementName = elementName;
+        this.dataStateWriter = dataStateWriter;
+        this.propertyGetter = propertyGetter;
+    }
+
+    public async Task WriteAsync(XmlWriter writer, TParent parent, string isoNamespace)
+    {
+        var optionalValue = propertyGetter(parent);
+        if ( optionalValue is TMember nonNullValue)
+        {
+            await writer.WriteStartElementAsync(null, this.elementName,isoNamespace);
+            await dataStateWriter.WriteAsync(writer, nonNullValue, isoNamespace);
+            await writer.WriteEndElementAsync();
+        }
+    }
+}
+
+
+public abstract class ContainerWriter<TParent> : IBeneficialXmlWriter<TParent>
+{
+    private readonly IBeneficialXmlWriter<TParent>[] memberWriters;
+
+    protected ContainerWriter(params IBeneficialXmlWriter<TParent>[] memberWriters)
+    {
+        this.memberWriters = memberWriters;
+    }
+
+    public async Task WriteAsync(XmlWriter writer, TParent value, string isoNamespace)
+    {
+        foreach( var memberWriter in memberWriters) await memberWriter.WriteAsync(writer,value,isoNamespace);
+    }
+}
+
+
+public class CustomerCreditTransferInitiationV11Serializer : ContainerWriter<CustomerCreditTransferInitiationV11>
+{
+    public CustomerCreditTransferInitiationV11Serializer(
+        // Dependency injection of subordinate writers
+        IBeneficialXmlWriter<GroupHeader95> groupHeader95writer,
+        IBeneficialXmlWriter<PaymentInstruction40> paymentInstructionWriter,
+        IBeneficialXmlWriter<SupplementaryData1> supplementalWriter
+    )
+    : base( 
+        new MemberWriter<CustomerCreditTransferInitiationV11, GroupHeader95>
+        ( 
+                "Gphdr", 
+                groupHeader95writer,
+                parent => parent.GroupHeader
+        ),
+        new MemberWriter<CustomerCreditTransferInitiationV11,PaymentInstruction40>
+        (
+                "PmtInstr",
+                paymentInstructionWriter,
+                parent => parent.PaymentInformation
+        ),
+        new MemberWriter<CustomerCreditTransferInitiationV11,SupplementaryData1>
+        (
+            "Supp",
+            supplementalWriter,
+            parent => parent.SupplementaryData
+        )
+    )
+    {
+    }
+}
+
+public class NullableIsoXmlWriter<T> : IBeneficialXmlWriter<T?>
+    where T : struct
+{
+    private readonly IBeneficialXmlWriter<T> nonNullableWriter;
+
+    public NullableIsoXmlWriter(IBeneficialXmlWriter<T> nonNullableWriter)
+    {
+        this.nonNullableWriter = nonNullableWriter;
+    }
+
+    public async Task WriteAsync(XmlWriter writer, T? value, string isoNamespace)
+    {
+        if ( value is T populatedValue) await this.nonNullableWriter.WriteAsync(writer,populatedValue,isoNamespace);
+    }
+}
+
+// Need SimpleMemberWriter, NullableMemberWriter
+
 
