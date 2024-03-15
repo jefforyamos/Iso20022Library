9126d2c98c6abb8b1de50a73516e7839d91cdb5e
Jeff Ward <jeff@jeffs-mdp.attlocal.net>
2024-03-15 08:02:06 -0500

    Made changes for 0.2.0.  Changed package id.

--- src/BeneficialStrategies.Iso20022.Common/Framework/IOuterRecord.cs
+++ src/BeneficialStrategies.Iso20022.Common/Framework/IOuterRecord.cs
@@ -8,36 +8,4 @@ namespace BeneficialStrategies.Iso20022.Framework
     public interface IOuterRecord
     {
     }
-
-
-    /// <summary>
-    /// Should adorn the implemenation of the top level record that has the main message data.
-    /// </summary>
-    /// <typeparam name="TSelf">Data type being decorated with the interface.</typeparam>
-    /// <typeparam name="TMessageDocType">Document wrapper type that will be instanced.</typeparam>
-    public interface IOuterRecord<TSelf, TMessageDocType> : IOuterRecord
-        where TMessageDocType : IOuterDocument<TSelf>
-        where TSelf : IOuterRecord
-    {
-        /// <summary>
-        /// Envelope this message inside it's serialization wrapper.
-        /// </summary>
-        /// <returns>Returns an outer document with this message as it's message payload.</returns>
-        TMessageDocType ToDocument();
-
-    }
-
-
-#if DECLARE_INTERNALSERIALIZATION
-    public interface ISerializeInsideARootElement
-    {
-           /// <summary>
-        /// Returns the normal root element for this record type.
-        /// </summary>
-        static abstract XName RootElement { get; }
-
-        static abstract string IsoXmlNamspace { get; }
-
-    }
-#endif
 }
